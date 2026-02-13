ce14c8b51c4560a66287385364b5f29bf9a38889
MatiasSantucho13 <matias.santucho@gmail.com>
2026-02-12 23:35:28 -0300

    Change model name from 'gemini-1.5-flash-001' to 'gemini-pro'

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ Si te preguntan precios, decí que depende del proyecto y pedí un contacto.
 """
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash-001",
+    model_name="gemini-pro",
     generation_config=generation_config,
     system_instruction=system_instruction,
 )
